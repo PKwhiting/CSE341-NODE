4187cc947cb06e79fc41e3653162aea8c99d1dad
PKwhiting <patten.whiting@gmail.com>
2023-01-12 09:39:57 -0700

    personal assignment 2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const routes = require('express').Router();
 
+
 routes.get('/', (req, res) => {
   res.send('Caleb Hansen');
 });
+routes.use('/contacts', require('./contacts'))
 
 module.exports = routes;
