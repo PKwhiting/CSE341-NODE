18bb2e5c6079c19d6e151699921401532cc5a717
PKwhiting <patten.whiting@gmail.com>
2023-01-12 10:35:10 -0700

    mongodb issues

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
-const routes = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
+router.use('/contacts', require('./contacts'))
 
-routes.get('/', (req, res) => {
-  res.send('Caleb Hansen');
-});
-routes.use('/contacts', require('./contacts'))
-
-module.exports = routes;
+module.exports = router;
